25063b42bdc0d006750e071dcdc0875b47977f91
NZAlona <amakoveychenko@gmail.com>
2023-01-17 10:54:39 +0200

    Adds load-more button

--- src/index.js
+++ src/index.js
@@ -3,21 +3,50 @@ import axios from 'axios';
 
 const formSearch = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
 formSearch.addEventListener('submit', onFormSubmit);
+loadBtn.addEventListener('click', onBtnClick);
+let page = 1;
+let searchQuery = '';
 
 async function onFormSubmit(event) {
   event.preventDefault();
   const form = event.currentTarget;
-  const searchQuery = form.elements.searchQuery.value.trim();
+  searchQuery = form.elements.searchQuery.value.trim();
 
   if (!searchQuery) {
+    Notiflix.Notify.failure('An empty input field. Please fill it up');
     return;
   }
 
+  loadBtn.classList.add('hidden');
+
   gallery.innerHTML = '';
-  const reply = await pixabayFetch(searchQuery);
+
+  const reply = await pixabayFetch(searchQuery, page);
+
+  const hits = reply.hits.length;
+
+  if (!hits) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  const totalHits = reply.totalHits;
+  if (totalHits > 0) {
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+
   cardsRender(reply);
+  loadBtn.classList.remove('hidden');
+  if (page * 40 > totalHits) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
 
 async function pixabayFetch(searchQuery, page = 1) {
@@ -28,7 +57,7 @@ async function pixabayFetch(searchQuery, page = 1) {
       `${BASE_URL}?key=${KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
     );
     const resp = await fetchResponse.data;
-    console.log(resp);
+
     return resp;
   } catch (error) {
     console.log(error);
@@ -65,5 +94,19 @@ function cardsRender(data) {
 </div>`
     )
     .join('');
-  gallery.insertAdjacentHTML('afterbegin', markup);
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+async function onBtnClick() {
+  page += 1;
+
+  const data = await pixabayFetch(searchQuery, page);
+
+  cardsRender(data);
+  if (page * 40 > data.totalHits) {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
+    loadBtn.classList.add('hidden');
+  }
 }
